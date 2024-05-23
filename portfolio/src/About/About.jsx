ced17b508a77e494b36da52f1a0629f984d1f4f0
bahador <noor.bahadory786@gmail.com>
2024-05-23 01:14:49 +0100

    created responsive part

--- portfolio/src/About/About.jsx
+++ portfolio/src/About/About.jsx
@@ -6,13 +6,13 @@ function About() {
         <img className="img" src="photo.jpeg" alt="" />
       </div>
       <div className="summery">
-        <p className="p-tag">Hello, my name is</p>
+        <p>Hello, my name is</p>
         <h1 className="name">Ahmadzay Bahadory</h1>
-        <hi>
+        <h1 className="summery-text">
           Versatile Full-Stack Developer continuing to further my education,
           excellent communication skills, a positive attitude towards challenges
           and strong cultural awareness.
-        </hi>
+        </h1>
         <div>
           <button className="hire-btn">
             <a
